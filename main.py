eeac7260d55e7c6b51377f8781ca165161eb1a0e
LanceC93 <42276366+LanceC93@users.noreply.github.com>
2022-10-01 17:40:20 -0400

    Update main.py
    either player can win the game now

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ def display_board(board):
     print("---------")
     print(board[0][0] + " | " + board[0][1] + " | " + board[0][2])
 
+#gives the results
+def display_results(turn):
+    if(turn > 8):
+        print("Draw!")
+    elif(turn % 2 == 1):
+        print("Player 1 Wins!")
+    else: 
+        print("Player 2 Wins!")
+
 #gives introduction 
 def intro():
     print("Welcome to Lance's EPIC Tic Tac Toe. Standard Tic Tac Toe rules. Use numpad numbers to select which square to fill.")
@@ -28,7 +37,6 @@ def play_game(turn, board):
 
     #assuming an int between 1-9 is provided (could be fixed to not allow those inputs)
     space = int(input("What space do you want to fill?\n"))
-
     #represents space in [row,col] format
     conv_space = convert_numpad(space)
 
@@ -43,23 +51,49 @@ def play_game(turn, board):
     else:
         board[conv_space[0]][conv_space[1]] = "O"
 
-#verifys if the game is over and returns false boolean value if it is
+#verifys if the game is over and returns false boolean value if it is (could be a better algorithm, could be worth returning to)
 def verify(turn, board):
-    if(turn > 9):
+    #checks if the board has been filled with no winner
+    if(turn > 8):
         return 0
+
+    #checks columns for a win
+    for i in range(0,3):
+        if(board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][0] != " "):
+            return 0
+        
+    #checks rows for a win
+    for i in range(0,3):
+        if(board[0][i] == board[1][i] and board[1][i] == board[2][i] and board[0][i] != " "):
+            return 0
+        
+    #checks diagonals
+    front_diagonal = board[0][0] == board[1][1] and board[1][1] == board[2][2]
+    back_diagonal = board[2][0] == board[1][1] and board[1][1] == board[0][2]
+    if((front_diagonal or back_diagonal) and board[1][1] != " "):
+        return 0
+    
+    #returns true if none of the win conditions have been met
     return 1
 
 def main():
     #give instructions
     intro()
+
     #records the turn number
     turn = 1
     #represents the blank spaces of the board
-    board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+    board = [[" "," "," "],[" "," "," "],[" "," "," "]]
+
     #continues playing the game until it is a win or draw
+    play_game(turn, board)      #if this isnt in front of the loop the final board does not display the final move made
     while (verify(turn, board)):
-        play_game(turn, board)
         turn += 1
+        play_game(turn, board)
+
+    #gives final board and win message
+    display_board(board)
+    display_results(turn)
 
 if __name__ == "__main__":
     main()
